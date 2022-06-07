c991ddf0c12030db54912ce168159f11c5787a84
lorenzoea12 <92109963+lorenzoea12@users.noreply.github.com>
2022-06-07 13:14:39 +0200

    dao piloto

--- ProyectoProgramacionFinal/src/main/java/Clases/Piloto.java
+++ ProyectoProgramacionFinal/src/main/java/Clases/Piloto.java
@@ -1,5 +1,6 @@
 package Clases;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -20,24 +21,45 @@ public class Piloto extends Persona {
 		super(nombre, edad, apellido, nacionalidad, dni);
 		
 		Statement smt = ConexionBD.conectar();
-		if(smt.executeUpdate("insert into persona (equipo,n,edad,apellido,nacionalidad) values  ('"
-				+equipo.getNombre()+"','"+coche.getNombre()+"','"+resistencia+"','"+experiencia+"')")>0) {
+		if(smt.executeUpdate("insert into persona (nombre,edad,apellido,nacionalidad,dni,equipo,coche,edad,apellido,nacionalidad) values  ('"
+				+nombre+"','"+edad+"','"+apellido+"','"+nacionalidad+"','"+dni+"','"+equipo.getNombre()+"','"+coche.getNombre()+"','"+resistencia+"','"+experiencia+"')")>0) {
 			this.equipo = equipo;
 			this.coche = coche;
 			this.resistencia = resistencia;
 			this.experiencia = experiencia;
+		
+			
+		}else {
 			ConexionBD.desconectar();
+			throw new SQLException("Error al insertar datos.");
 			
 		}
 	
+	}	
+	public Piloto(String nombre) {
+		
+	
+	Statement smt = ConexionBD.conectar();
+	
+	
+		ResultSet cursor = smt.executeQuery("SELECT * FROM piloto WHERE nombre =" + nombre+";");
+		
+		if (cursor.next() ) {
+			
+		}
+			this.resistencia = cursor.getByte(resistencia);
+			this.experiencia = cursor.getByte(experiencia);
+			
+	
+
+}
+
+		
 	
 		
 		
+		
 	
-		
-		
-		
-	}
 
 
 	public Equipo getEquipo() {
@@ -45,9 +67,20 @@ public class Piloto extends Persona {
 	}
 	
 	
-	public void setEquipo(Equipo equipo) {
-		this.equipo = equipo;
-	}
+	public void setEquipo(Equipo equipo) throws SQLException {
+		Statement smt=ConexionBD.conectar();
+		if(smt.executeUpdate("Update piloto set equipo='"+equipo+"' where nombre = "+this.nombre+"")>0) {
+			this.equipo=equipo;
+		
+		}
+		else {
+			ConexionBD.desconectar();
+			throw new SQLException("No se pudo cambiar el equipo del piloto");
+		}
+		ConexionBD.desconectar();
+}
+		
+	
 	
 	
 	public Coche getCoche() {
@@ -55,9 +88,20 @@ public class Piloto extends Persona {
 	}
 	
 	
-	public void setCoche(Coche coche) {
-		this.coche = coche;
-	}
+	public void setCoche(Coche coche) throws SQLException {
+		Statement smt=ConexionBD.conectar();
+		if(smt.executeUpdate("Update piloto set coche='"+coche+"' where nombre = "+this.nombre+"")>0) {
+			this.coche=coche;
+		
+		}
+		else {
+			ConexionBD.desconectar();
+			throw new SQLException("No se pudo cambiar el coche del piloto");
+		}
+		ConexionBD.desconectar();
+}
+		
+	
 	
 	
 	public byte getResistencia() {
@@ -65,9 +109,20 @@ public class Piloto extends Persona {
 	}
 	
 	
-	public void setResistencia(byte resistencia) {
-		this.resistencia = resistencia;
-	}
+	public void setResistencia(byte resistencia) throws SQLException {
+		Statement smt=ConexionBD.conectar();
+		if(smt.executeUpdate("Update piloto set resistencia='"+resistencia+"' where nombre = "+this.nombre+"")>0) {
+			this.resistencia=resistencia;
+		
+		}
+		else {
+			ConexionBD.desconectar();
+			throw new SQLException("No se pudo cambiar la resistencia del piloto");
+		}
+		ConexionBD.desconectar();
+}
+	
+	
 	
 	
 	public byte getExperiencia() {
@@ -75,9 +130,20 @@ public class Piloto extends Persona {
 	}
 	
 	
-	public void setExperiencia(byte experiencia) {
+	public void setExperiencia(byte experiencia) throws SQLException {
 		this.experiencia = experiencia;
-	}
+		Statement smt=ConexionBD.conectar();
+		if(smt.executeUpdate("Update piloto set experiencia='"+experiencia+"' where nombre = "+this.nombre+"")>0) {
+			this.experiencia=experiencia;
+		
+		}
+		else {
+			ConexionBD.desconectar();
+			throw new SQLException("No se pudo cambiar el coche del piloto");
+		}
+		ConexionBD.desconectar();
+}
+	
 	
 	
 	@Override
